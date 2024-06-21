c3e26cea594536ae23b52a81edc8202ea65919c9
vintee mishra <v.mishra@afidigitalservices.com>
2024-06-21 17:09:08 +0530

    commit with switch between langid and category id

--- server/api/module-categories/index.js
+++ server/api/module-categories/index.js
@@ -23,9 +23,9 @@ modulesCategories.post("/", post);
 modulesCategories.delete("/:moduleCategoryKey", del);
 modulesCategories.post("/publish", publish);
 modulesCategories.get("/publish/version", getPublishVersion);
-modulesCategories.get("/category_wise_data/:id/:langId",ModulesByCategory);
-modulesCategories.get("/category-wise-image-bundle.zip/:id/:langId",ZipByCategory);
-modulesCategories.get("/category-wise-video-bundle.zip/:id/:langId",ZipvideosByCategory);
+modulesCategories.get("/category_wise_data/:langId/:id",ModulesByCategory);
+modulesCategories.get("/category-wise-image-bundle.zip/:langId/:id",ZipByCategory);
+modulesCategories.get("/category-wise-video-bundle.zip/:langId/:id",ZipvideosByCategory);
 
 
 
